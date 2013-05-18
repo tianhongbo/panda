76485c586e0a391449624077522c197fd96562a1
hongbo tian <tianhongbo1@gmail.com>
2013-05-18 01:05:28 -0700

    first commit

---
+++ src/EpcBearer/Bearer.java
@@ -0,0 +1,14 @@
+package EpcBearer;
+
+public class Bearer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hello, I am the bearer");
+
+	}
+
+}
